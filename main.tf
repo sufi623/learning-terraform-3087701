f510c1412d39b485603b3b9814914c6fd87663c6
sufi623 <65691531+sufi623@users.noreply.github.com>
2025-04-19 15:35:11 -0400

    -

--- main.tf
+++ main.tf
@@ -18,13 +18,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
-
   tags = {
     Name = "HelloWorld"
   }
 }
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out"
